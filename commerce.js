9c1399fe2d2004dd61547f06579fe87538bd8718
nianod <arnoldkk422@gmail.com>
2025-03-07 11:53:21 +0300

    third commit

--- commerce.js
+++ commerce.js
@@ -1,4 +1,8 @@
-const buy = document.getElementById("buy-now");
+// const buy = document.getElementById("buy-now");
+// buy.addEventListener('click', function(){
+//     alert("Item successfully bought")
+// });
+const trash = document.getElementById("trash");
 buy.addEventListener('click', function(){
-    alert("Item successfully bought")
+    alert("Item successfully ")
 });
