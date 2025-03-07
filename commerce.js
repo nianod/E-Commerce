c7f741b486c4fe827bf39620a1e39d5d980707ca
nianod <arnoldkk422@gmail.com>
2025-03-07 22:00:27 +0300

    dynamic one

--- commerce.js
+++ commerce.js
@@ -1,17 +1,34 @@
 
 const cartIcon = document.querySelector("#cart-icon");
 const cart = document.querySelector(".cart");
-const body = document.getElementsByTagName("cart")
-const trash = document.getElementById("trash");
-const closeCart = document.getElementById("close-cart");
+const closeCart = document.querySelector("#close-cart");
 
 
 
 cartIcon.onclick = () => {
         cart.classList.add("active");
 }
-
+  
 closeCart.onclick = () => {
-    // cart.classList.remove("active");
-    body.sty
+        cart.classList.remove("active");
+}
+
+if(document.readyState === 'loading'){
+   document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready();
+}
+
+ready = () => {
+    let trash = document.querySelectorAll('.trash');
+    console.log(trash);
+    for(let i = 0; i < trash.length; i++) {
+        let button = trash[i]
+        button.addEventListener('click', removeCartItem);
+    }
+    
+}
+removeCartItem = (event) => {
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
 }
