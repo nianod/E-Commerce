08eb7b5f6ec5ff915a631d1ded08f6d61af54e2a
nianod <arnoldkk422@gmail.com>
2025-03-08 22:06:37 +0300

    dynamic func

--- commerce.js
+++ commerce.js
@@ -19,7 +19,7 @@ if(document.readyState === 'loading'){
     ready();
 }
 
-ready = () => {
+ready = () => { 
     let trash = document.querySelectorAll('.trash');
     console.log(trash);
     for(let i = 0; i < trash.length; i++) {
@@ -31,4 +31,9 @@ ready = () => {
 removeCartItem = (event) => {
     let buttonClicked = event.target;
     buttonClicked.parentElement.remove();
+}
+function updateTotal() {
+    let cartContent = document.getElementsByClassName("cart-content")[0]
+    let cartBoxes = cartContent.getElementsByClassName("cart-box")
+    for(cartBoxes = 0;cartBoxes < cart-cartBoxes.length; cartBoxes++ )
 }
